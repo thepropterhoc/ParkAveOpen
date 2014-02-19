322f1f4feb932133115e14ab438d3573f04f95dc
thepropterhoc <shelbyvanhooser@yahoo.com>
2014-02-19 14:17:46 -0600

    Signup interface is finished

---
+++ Parkly/Parkly/Files/DTLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  DTLoginViewController.h
+//  Parkly
+//
+//  Created by Shelby Vanhooser on 2/19/14.
+//  Copyright (c) 2014 DevTeam14. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTLoginViewController : UIViewController
+
+@end
---
+++ Parkly/Parkly/Files/DTLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  DTLoginViewController.m
+//  Parkly
+//
+//  Created by Shelby Vanhooser on 2/19/14.
+//  Copyright (c) 2014 DevTeam14. All rights reserved.
+//
+
+#import "DTLoginViewController.h"
+
+@interface DTLoginViewController ()
+
+@end
+
+@implementation DTLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
