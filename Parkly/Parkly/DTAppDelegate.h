fb68ba3a75ca2eb41ce07e89c3c9a3414ff4ba0f
thepropterhoc <shelbyvanhooser@yahoo.com>
2014-02-18 21:56:29 -0600

    Interface is Live

---
+++ Parkly/Parkly/DTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DTAppDelegate.h
+//  Parkly
+//
+//  Created by Shelby Vanhooser on 2/18/14.
+//  Copyright (c) 2014 DevTeam14. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Parkly/Parkly/DTViewController.h
@@ -0,0 +1,13 @@
+//
+//  DTViewController.h
+//  Parkly
+//
+//  Created by Shelby Vanhooser on 2/18/14.
+//  Copyright (c) 2014 DevTeam14. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTViewController : UIViewController
+
+@end
