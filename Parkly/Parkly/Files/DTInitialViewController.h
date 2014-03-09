58ec6e1d24c0f091bef725d910f8387b3879714f
thepropterhoc <shelbyvanhooser@yahoo.com>
2014-03-09 17:34:57 -0500

    Demo Tweaked

---
+++ Parkly/Parkly/Files/DTInitialViewController.h
@@ -0,0 +1,13 @@
+//
+//  DTInitialViewController.h
+//  Parkly
+//
+//  Created by Shelby Vanhooser on 3/9/14.
+//  Copyright (c) 2014 DevTeam14. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTInitialViewController : UIViewController
+
+@end
---
+++ Parkly/Parkly/Files/DTInitialViewController.m
@@ -0,0 +1,38 @@
+//
+//  DTInitialViewController.m
+//  Parkly
+//
+//  Created by Shelby Vanhooser on 3/9/14.
+//  Copyright (c) 2014 DevTeam14. All rights reserved.
+//
+
+#import "DTInitialViewController.h"
+
+@interface DTInitialViewController ()
+
+@end
+
+@implementation DTInitialViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
